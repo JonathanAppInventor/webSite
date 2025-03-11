d39f0203fb57654e1f3b183b8c04efef3a5d11cf
JonathanAppInventor <52981652+JonathanAppInventor@users.noreply.github.com>
2025-03-11 07:54:58 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,14 +11,14 @@ const translations = {
     title: "Hoja de Vida",
     description: "Esta foto fue creada en 2013, cuando esta realizando mi trabajo de grado y aun no existia la inteligencia artificial, por un compañero, para hacerme una broma.",
     content: "Ingeniero electrónico con cinco años de experiencia en desarrollo de software, desarrollo web FullSatck y administración de bases de datos SQL; ha trabajado en diseño, desarrollo y mantenimiento de sistemas electrónicos, orientados a telecomunicaciones especialmente Internet de las cosas (IOT). Habilidades avanzadas en programación de microcontroladores, circuitos integrados y diseño de (PCB). Gran capacidad de análisis, resolución de problemas técnicos, trabajo en equipo, buena presentación personal, inglés avanzado, altruista, proactivo, dinámico, positivo.",
-    end: "Sitio en construccion y mejora",
+    end: "Sitio en construccion y mejoras...",
     button: "Divertirse"
   },
   en: {
     title: "Welcome",
     description: "This picture was created in 2013, when i was doing my work of grade and there's no exist artificial intelligence, by a classmate, in order to make me a joke.",
     content: "Electronic engineer with five years of experience in software development, FullSatck web development and SQL database administration; has worked in the design, development and maintenance of electronic systems, oriented to telecommunications, especially the Internet of Things (IOT). Advanced skills in microcontroller programming, integrated circuits and PCB design. Great analytical skills, technical problem solving, teamwork, good personal presentation, advanced English, altruistic, proactive, dynamic, positive.",
-    end: "Site under building and improvements",
+    end: "Site under building and improvements...",
     button: "Have Fun"
   }
 };
